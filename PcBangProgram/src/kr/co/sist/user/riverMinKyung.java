f6e48e1a2c715367a9563dac98a20c395cca4c45
owner <owner@DESKTOP-FFGSL1S>
2019-08-28 11:32:46 +0900

    내가짱

---
+++ PcBangProgram/src/kr/co/sist/user/riverMinKyung.java
@@ -0,0 +1,13 @@
+package kr.co.sist.user;
+
+/**
+ * 반드시 team -> pull 하고 commit
+ * 기능생성은 master에서
+ * @author owner
+ */
+public class riverMinKyung {
+	public riverMinKyung() {
+		System.out.println("희희"); 
+	}
+	
+}
