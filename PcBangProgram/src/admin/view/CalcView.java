a43b9a834e3ad4284122dbd23cde56c54a898208
owner <owner@DESKTOP-6MMF91Q>
2019-09-20 19:42:34 +0900

    2019.09.20 19:42 윤소인 개 노가다 같은 일 2번 하는 개똥멍청이1!!!!!!!!!!!!!!

--- PcBangProgram/src/admin/view/CalcView.java
+++ PcBangProgram/src/admin/view/CalcView.java
@@ -1,17 +1,23 @@
 package admin.view;
 
 import java.awt.BorderLayout;
+import java.awt.Checkbox;
+import java.awt.CheckboxGroup;
 
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.border.TitledBorder;
 import javax.swing.table.DefaultTableModel;
 
 import admin.controller.CalcItemEvt;
 import admin.controller.CalcPCEvt;
+import javafx.scene.control.CheckBox;
 
 @SuppressWarnings("serial")
 public class CalcView extends JPanel {
@@ -60,22 +66,42 @@ public class CalcView extends JPanel {
 		jbtCalcPC = new JButton("PC 정산") ;
 		jbtCalcItem = new JButton("매점 정산") ;
 		
-//		JPanel jpCalc = new JPanel() ;
-//		jpCalc.setLayout(new BorderLayout()) ;
-//		jpCalc.setBorder(new TitledBorder("PC사용료 정산")) ;
 		
 		JScrollPane jspCalcPC = new JScrollPane(jtCalcPC);
 		JPanel jpCalcPC = new JPanel();
+		JPanel jpCalcPC2 = new JPanel();
 		JPanel jpCalcPCSouth = new JPanel();
 		
 		jpCalcPC.setLayout(new BorderLayout()) ;
 		jpCalcPC.setBorder(new TitledBorder("PC사용료 정산")) ;
 		
-//		jpCalcPC.add(jspCalcPC);
-//		jpCalcPCSouth.add(jbtClosePC) ;
+		JPanel jpCalcPCNorth = new JPanel() ;
+		jpCalcPCNorth.setLayout(new BorderLayout()) ;
+		jpCalcPCNorth.setBorder(new TitledBorder("날짜 옵션")) ;
+		
+		CheckboxGroup cgPC = new CheckboxGroup() ;
+		Checkbox chbTodayPC = new Checkbox("오늘", cgPC, true);
+		Checkbox chbWeekPC = new Checkbox("일주일", cgPC, false);
+		Checkbox chbMonthPC = new Checkbox("한 달", cgPC, false);
+		Checkbox chbCustomPC = new Checkbox("사용자 지정", cgPC, false);
+		
+		JTextField jtfStartPC = new JTextField(15) ;
+		JLabel jlblPC = new JLabel("~") ;
+		JTextField jtfEndPC = new JTextField(15) ;
+		
+		jpCalcPC2.add(chbTodayPC) ;
+		jpCalcPC2.add(chbWeekPC) ;
+		jpCalcPC2.add(chbMonthPC) ;
+		jpCalcPC2.add(chbCustomPC) ;
+		jpCalcPC2.add(jtfStartPC) ;
+		jpCalcPC2.add(jlblPC) ;
+		jpCalcPC2.add(jtfEndPC) ;
+		
+		jpCalcPCNorth.add(jpCalcPC2) ;
 		jpCalcPCSouth.add(jbtCalcPC) ;
 		jpCalcPC.add("Center", jspCalcPC) ;
 		jpCalcPC.add("South", jpCalcPCSouth) ;
+		jpCalcPC.add("North",jpCalcPCNorth) ;
 		
 //		jpCalcPC.setLayout(null);
 		jspCalcPC.setBounds(0, 0, 700, 600);
@@ -92,15 +118,42 @@ public class CalcView extends JPanel {
 		
 		JScrollPane jspCalcItem = new JScrollPane(jtCalcItem) ;
 		JPanel jpCalcItem = new JPanel() ;
+		JPanel jpCalcItem2 = new JPanel() ;
 		JPanel jpCalcItemSouth = new JPanel() ;
+		JPanel jpCalcItemNorth = new JPanel() ;
 		
 		jpCalcItem.setLayout(new BorderLayout()) ;
 		jpCalcItem.setBorder(new TitledBorder("매점 정산"));
+		
+		jpCalcItemNorth.setLayout(new BorderLayout()) ;
+		jpCalcItemNorth.setBorder(new TitledBorder("날짜 옵션")) ;
+		
+		CheckboxGroup cgItem = new CheckboxGroup() ;
+		Checkbox chbTodayItem = new Checkbox("오늘", cgItem, true);
+		Checkbox chbWeekItem = new Checkbox("일주일", cgItem, false);
+		Checkbox chbMonthItem = new Checkbox("한 달", cgItem, false);
+		Checkbox chbCustomItem = new Checkbox("사용자 지정", cgItem, false);
+		
+		JTextField jtfStartItem = new JTextField(15) ;
+		JLabel jlblItem = new JLabel("~") ;
+		JTextField jtfEndItem = new JTextField(15) ;
+		
+		jpCalcItem2.add(chbTodayItem) ;
+		jpCalcItem2.add(chbWeekItem) ;
+		jpCalcItem2.add(chbMonthItem) ;
+		jpCalcItem2.add(chbCustomItem) ;
+		jpCalcItem2.add(jtfStartItem) ;
+		jpCalcItem2.add(jlblItem) ;
+		jpCalcItem2.add(jtfEndItem) ;
+		
+		jpCalcItemNorth.add(jpCalcItem2) ;
+		
 		
 //		jpCalcItemSouth.add(jbtCloseItem) ;
 		jpCalcItemSouth.add(jbtCalcItem) ;
 		jpCalcItem.add("Center", jspCalcItem) ;
 		jpCalcItem.add("South", jpCalcItemSouth) ;
+		jpCalcItem.add("North", jpCalcItemNorth) ;
 		
 		
 		jspCalcItem.setBounds(0, 0, 700, 600);
