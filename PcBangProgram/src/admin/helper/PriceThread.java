5e65ac23209b19a1037446cd8cae7f7eca660957
owner <owner@DESKTOP-3252VA6>
2019-09-26 17:04:42 +0900

    09-26 15:04 김호진

---
+++ PcBangProgram/src/admin/helper/PriceThread.java
@@ -0,0 +1,33 @@
+package admin.helper;
+
+import javax.swing.JLabel;
+
+public class PriceThread extends Thread{
+	
+	private JLabel jlUsePrice;
+	
+	public PriceThread(JLabel jlUsePrice) {
+		this.jlUsePrice=jlUsePrice;
+		
+	}//PriceThread
+	
+	@Override
+	public void run() {
+		
+		
+		int price=0; ////5분당 100원 가격
+		while(true) {
+			price=price+100;
+			//사용금액 thread
+			
+			jlUsePrice.setText("사용 요금: "+String.valueOf(price)+"원");
+			try {
+				Thread.sleep(300*1000); //5분마다 가격 증가
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}//end catch 
+		}//end while
+		
+	}//run
+
+}//class
